783ec1a59e2b835693ab1210b65411e1edac9cab
Caroline Goulart <goulartscaroline@gmail.com>
2020-08-16 13:54:55 -0300

    NodeJs challenge resolution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,24 +6,52 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface ReceiptDTO {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+  private balance: Balance;
+  private receipt: ReceiptDTO;
 
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
+    this.receipt = { transactions: this.all(), balance: this.getBalance() };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO ): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    if(type === "income") {
+      this.balance.income += value;
+    } else {
+      this.balance.outcome += value;
+    }
+    this.balance.total = this.balance.income - this.balance.outcome;
+    return transaction;
   }
+
+  public getReceipt(): ReceiptDTO {
+    return this.receipt;
+  }
+
 }
 
 export default TransactionsRepository;
